e9dd8ad9e947559ee5aae22d61d60c5474c824de
Paul Chelarescu <paul-constantin.chelarescu@student.manchester.ac.uk>
2017-11-25 19:33:03 +0000

    Added dummy data

--- get_routeid_by_tweet.py
+++ get_routeid_by_tweet.py
@@ -1,3 +1,7 @@
+routes = [[1, [[51.4952103,-0.1460866], [50.8296233,-0.143276]]],
+        [2, [[51.5052103,-0.1560866], [50.4396233,-0.113276]]]]
+tweet = {"id" : 3, "created_at" : "Wed Aug 27 14:08:45 +0000 2008",
+        "text" : "this is a tweet", "coordinates" : [51.087290, -0.160528]}
 def get_routeid_by_tweet(routes, tweet):
     """
     INPUTS
@@ -7,3 +11,4 @@ def get_routeid_by_tweet(routes, tweet):
     OUTPUT
     routeid of best guess from the tweet
     """
+    
