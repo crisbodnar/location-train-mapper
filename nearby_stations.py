d9f3f8dce6ee8210becac4f86739b60f00c9908c
Cristian Bodnar <cristian.bodnar@manchester.ac.uk>
2017-11-25 17:53:48 +0000

    Some shit which doesn't work yet

--- nearby_stations.py
+++ nearby_stations.py
@@ -5,13 +5,16 @@ import urllib.request
 
 data_link = "http://journeyplanner.silverrailtech.com/journeyplannerservice/v2/REST/DataSets/UKNationalRailRT/NearbyTransitStops?ApiKey=1333ECBD-2A86-08A5-7168-D325C905A731&GeoCoordinate=52.21650468937126%2C-1.415133326455713&MaximumDistanceInMetres=30000&WalkSpeed=&MaximumStopsToReturn=&ReturnPolylineInformation=False&Time=2017-11-25T11%3A45&TimeBand=60&FilterInactiveStops=True&TransactionId=&format=json"
 with urllib.request.urlopen(data_link) as url:
-	data = json.loads(url.read().decode())
+    data = json.loads(url.read().decode())
 
 transit_stop_paths = data["TransitStopPaths"]
-routes = []
+uids = []
 
 for path in transit_stop_paths:
-	stop = path["TransitStop"]
-	routes += stop["Routes"].split(";")
-	
-routes_array = set(routes)
+    trips = path["Trips"]
+    for trip in trips:
+        summary = trip['Summary']
+        uids.append(summary['TripUid'])
+
+uids = set(uids)
+print(uids)
