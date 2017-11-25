8cf28777339251e4a6578c18d34c9e763da3ef84
Paul Chelarescu <paul-constantin.chelarescu@student.manchester.ac.uk>
2017-11-25 16:17:36 +0000

    Indentation

--- approximate_time.py
+++ approximate_time.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3.6
 
 
- def approximate_time(start_co, start_time, finish_co, finish_time, input_co):
- 	"""
- 	INPUTS
- 	start: coordinates of start
- 	finish: coordinates of finish
- 	OUTPUT
- 	coordinates | INPUTS
- 	"""
+def approximate_time(start_co, start_time, finish_co, finish_time, input_co):
+    """
+    INPUTS
+    start: coordinates of start
+    finish: coordinates of finish
+    OUTPUT
+    coordinates | INPUTS
+    """
 
 
