3a8d6a907ab49bc8e475fd10b82f040836d99a78
Paul Chelarescu <paul-constantin.chelarescu@student.manchester.ac.uk>
2017-11-25 19:12:05 +0000

    Renamed input_co to tweet_co

--- approximate_time.py
+++ approximate_time.py
@@ -5,27 +5,27 @@ start_co = [51.4952103,-0.1460866]
 finish_co = [50.8296233,-0.143276]
 start_time = "Wed Aug 27 13:08:45 +0000 2008"
 finish_time = "Wed Aug 27 14:08:45 +0000 2008"
-input_co = [51.087290, -0.160528]
+tweet_co = [51.087290, -0.160528]
 
-def approximate_time(start_co, start_time, finish_co, finish_time, input_co):
+def approximate_time(start_co, start_time, finish_co, finish_time, tweet_co):
     """
     INPUTS
     start_co: coordinates of start location
     finish_co: coordinates of finish location
     start_time: start time in epoch
     finish_time: finish time in epoch
-    input_co: coordinates of input location
+    tweet_co: coordinates of input location
 
     OUTPUT
     time of location in epoch
     """
-    delta = (finish_co[0] - input_co[0]) / (input_co[0] - start_co[0])
+    delta = (finish_co[0] - tweet_co[0]) / (tweet_co[0] - start_co[0])
     return (finish_time + start_time * delta) / (1 + delta)
 
 start_time_epoch = datetime.strptime(start_time, "%a %b %d %H:%M:%S %z %Y").timestamp()
 finish_time_epoch = datetime.strptime(finish_time, "%a %b %d %H:%M:%S %z %Y").timestamp()
 
-query_time_epoch = (approximate_time(start_co, start_time_epoch, finish_co, finish_time_epoch, input_co))
+query_time_epoch = (approximate_time(start_co, start_time_epoch, finish_co, finish_time_epoch, tweet_co))
 
 query_time = (datetime.utcfromtimestamp(query_time_epoch).strftime("%a %b %d %H:%M:%S %z %Y"))
 
