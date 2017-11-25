a167f927b0839f59ccef0e96b77dff8c44018754
intibeer <intibeer@googlemail.com>
2017-11-25 16:58:44 +0000

    updated date format

--- approximate_time.py
+++ approximate_time.py
@@ -2,18 +2,31 @@
 
 start_co = [51.4952103,-0.1460866]
 end_co = [50.8296233,-0.143276]
-start_time = [16:57]
-finish_time = [15:57]
+start_time = ["16:57"]
+finish_time = ["15:57"]
 input_co = [51.087290, -0.160528]
+
+tweet_time_format = "Thu Nov 16 16:25:18 +0000 2017"
+
+
+av_speed = int
+
+total_time = 
 
 def approximate_time(start_co, start_time, finish_co, finish_time, input_co):
     """
     INPUTS
     start: coordinates of start
     finish: coordinates of finish
+
     OUTPUT
     coordinates | INPUTS
+
     """
+    def format_time(time):
+    	#reformat "15:57" -> 15:57:00
+    	time += ":00"
+    	return time 
 
 
 
