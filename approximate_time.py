badb4af0cfe669c1bac27232b01091b72e580207
intibeer <intibeer@googlemail.com>
2017-11-25 16:51:31 +0000

    dummy coordinates

--- approximate_time.py
+++ approximate_time.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3.6
 
+start_co = [51.4952103,-0.1460866]
+end_co = [50.8296233,-0.143276]
+start_time = [16:57]
+finish_time = [15:57]
+input_co = [51.087290, -0.160528]
 
 def approximate_time(start_co, start_time, finish_co, finish_time, input_co):
     """
@@ -9,5 +14,6 @@ def approximate_time(start_co, start_time, finish_co, finish_time, input_co):
     OUTPUT
     coordinates | INPUTS
     """
+
 
 
